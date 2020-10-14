e0eef649ae1e445b606e3741625a9ae4bf6ccf5a
Martin Anseaume <martin.anseaume@siren.io>
2020-10-14 15:25:48 +0200

    Try reflection by default on JDK <= 15

--- common/src/main/java/io/netty/util/internal/PlatformDependent0.java
+++ common/src/main/java/io/netty/util/internal/PlatformDependent0.java
@@ -820,7 +820,7 @@ final class PlatformDependent0 {
 
     private static boolean explicitTryReflectionSetAccessible0() {
         // we disable reflective access
-        return SystemPropertyUtil.getBoolean("io.netty.tryReflectionSetAccessible", javaVersion() < 15);
+        return SystemPropertyUtil.getBoolean("io.netty.tryReflectionSetAccessible", javaVersion() < 16);
     }
 
     static boolean isExplicitTryReflectionSetAccessible() {
