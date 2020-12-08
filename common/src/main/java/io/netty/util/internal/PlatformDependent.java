6248581b102a4f63d5a64d128ae5c261c7107e15
Johnny Hujol <johnny.hujol@gmail.com>
2020-12-08 16:30:28 +0100

    Add capability to set the max direct memory from a static field and enforce it optionally

--- common/src/main/java/io/netty/util/internal/PlatformDependent.java
+++ common/src/main/java/io/netty/util/internal/PlatformDependent.java
@@ -112,6 +112,10 @@ public final class PlatformDependent {
         }
     };
 
+    /**
+     * @deprecated use {@link PlatformDependentCompanion} to set and get the max direct memmory instead of this
+     * Java system property.
+     */
     public static final String JAVA_SYS_PROP_IO_NETTY_MAX_DIRECT_MEMORY = "io.netty.maxDirectMemory";
 
     static {
@@ -140,19 +144,41 @@ public final class PlatformDependent {
          */
         if (!hasUnsafe() && !isAndroid() && !PlatformDependent0.isExplicitNoUnsafe()) {
             logger.info(
-                    "Your platform does not provide complete low-level API for accessing direct buffers reliably. " +
-                    "Unless explicitly requested, heap buffer will always be preferred to avoid potential system " +
-                    "instability.");
+              "Your platform does not provide complete low-level API for accessing direct buffers reliably. " +
+                "Unless explicitly requested, heap buffer will always be preferred to avoid potential system " +
+                "instability.");
         }
 
-        // Here is how the system property is used:
-        //
-        // * <  0  - Don't use cleaner, and inherit max direct memory from java. In this case the
-        //           "practical max direct memory" would be 2 * max memory as defined by the JDK.
-        // * == 0  - Use cleaner, Netty will not enforce max memory, and instead will defer to JDK.
-        // * >  0  - Don't use cleaner. This will limit Netty's total direct memory
-        //           (note: that JDK's direct memory limit is independent of this).
-        long maxDirectMemory = SystemPropertyUtil.getLong(JAVA_SYS_PROP_IO_NETTY_MAX_DIRECT_MEMORY, -1);
+        // Make sure that 3rd party library had the chance to set the max direct memory prior to getting in here.
+        try {
+            PlatformDependentCompanion.checkSetMaxDirectMemoryWasCalled();
+        } catch (IllegalAccessException e) {
+            logger.error("To avoid this exception please either let Netty use the default value for " +
+              "the max direct memory by calling PlatformDependentCompanion.setUseDefaultMaxDirectMemory() or call " +
+              "PlatformDependentCompanion.setMaxDirectMemory(long m) to tell Netty the max direct memory to use here.");
+            throwException(e);
+        }
+
+        long maxDirectMemory = PlatformDependentCompanion.getMaxDirectMemory();
+        if (maxDirectMemory < 0) {
+            // Here is how the system property is used:
+            //
+            // * <  0  - Don't use cleaner, and inherit max direct memory from java. In this case the
+            //           "practical max direct memory" would be 2 * max memory as defined by the JDK.
+            // * == 0  - Use cleaner, Netty will not enforce max memory, and instead will defer to JDK.
+            // * >  0  - Don't use cleaner. This will limit Netty's total direct memory
+            //           (note: that JDK's direct memory limit is independent of this).
+            maxDirectMemory = SystemPropertyUtil.getLong(JAVA_SYS_PROP_IO_NETTY_MAX_DIRECT_MEMORY, -1);
+
+            if (maxDirectMemory >= 0) {
+                logger.warn("Using java system property '{}' is deprecated and will be removed in the future in favor" +
+                    " of using one of the setter methods from the class '{}'" ,
+                  JAVA_SYS_PROP_IO_NETTY_MAX_DIRECT_MEMORY, PlatformDependentCompanion.class.getName());
+            }
+        } else {
+            logger.info("max direct memory was defined using a setter method from the class '{}'" ,
+              PlatformDependentCompanion.class.getName());
+        }
 
         if (maxDirectMemory < 0) {
           maxDirectMemory = MAX_DIRECT_MEMORY;
